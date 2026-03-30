3f53009aa6b13cae419e25523ff4ead585f6ae87
sabishii-bit <cmsheets@pm.me>
2026-03-29 19:37:31 -0700

    waybar: scripting updated. brave: keychain changed

--- .config/hypr/scripts/rename-workspaces.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Rename DP-2's workspaces (11-20) to display as 1-10
-# Delay to ensure hyprsplit has created all workspaces before renaming
-sleep 2
-for i in $(seq 1 10); do
-    hyprctl dispatch renameworkspace $((i + 10)) "$i"
-done
